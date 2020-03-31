61f8dcc9115e649703b7df644424087f9d7713ad
BuildTools <unconfigured@null.spigotmc.org>
2020-03-31 17:31:55 -0500

    Finished

--- src/Generator.java
+++ src/Generator.java
@@ -1,12 +1,31 @@
 public class Generator {
     public static void main(String[] args) {
 
-        // An example of how to play M1
+        Minuet m = new Minuet();
+        m.calculateRolls();
+        Trio t = new Trio();
+        t.calculateRolls();
+        for(int i = 1; i <= 32; i++) //play song
+        {
 
-        for(int i = 1; i <= 172; i++) {
-            double[] sample = StdAudio.read("mozart/M"+i+".wav");
-            StdAudio.play(sample);
+            if(i < 17)
+            {
+                double[] minuet = StdAudio.read("mozart/M"+m.minuetParts[m.getMinuetRolls()[i-1]][i-1]+".wav");
+                System.out.print(m.getMinuetRolls()[i-1]);
+                System.out.print(" " + m.minuetParts[m.getMinuetRolls()[i-1]][i-1]);
+                System.out.println(" "+i);
+                StdAudio.play(minuet);
+            }
+            else
+            {
+                double[] trio = StdAudio.read("mozart/M"+t.trioParts[t.getTrioRolls()[i-17]][i-17]+".wav");
+                System.out.print(t.getTrioRolls()[i-17]);
+                System.out.print(" " +t.trioParts[t.getTrioRolls()[i-17]][i-17]);
+                System.out.println(" "+i);
+                StdAudio.play(trio);
+            }
         }
 
     }
+
 }
